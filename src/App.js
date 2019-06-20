a5d6313b7bdc35c8373f94153620933e2d14d772
Marcos Sevilla <marcossevilla14@gmail.com>
2019-06-20 14:42:36 -0600

    added alert state and component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   async componentDidMount() {
@@ -31,11 +33,17 @@ class App extends Component {
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
+
     this.setState({ users: res.data.items, loading: false });
   };
 
   // clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
 
   render() {
     const { users, loading } = this.state;
@@ -44,10 +52,12 @@ class App extends Component {
       <div className='app'>
         <Navbar title='GitHub Finder' icon='fab fa-github' />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
